541ea77c7b2dfc1f7c54a3e775621b0854ff6b91
Jamie Tardi <jamie@Jamies-MacBook-Pro.local>
2021-08-08 11:49:40 +0100

    api added

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import { popularGamesURL } from './api';
+
+function App() {
+	console.log(popularGamesURL());
+	return <div className='App'></div>;
+}
+
+export default App;
